e269cff89e4c3a10fbbbc0ea1c7cc33119287222
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-04 18:22:22 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    
+    answer = strings;
+    sort(answer.begin(), answer.end(), [n](const string& a, const string& b) { return a[n] == b[n] ? a < b : a[n] < b[n]; });
+    
+    return answer;
+}
