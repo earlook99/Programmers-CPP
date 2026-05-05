8027792f551f20141a6ba08898c057b844ca58c0
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-05 16:29:00 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 5.83 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    for (long long i = 1; i <= n; i++)
+    {
+        if (i*i == n) 
+        {
+            answer = (i+1)*(i+1);
+            break;
+        }
+        else if (i*i > n)
+        {
+            answer = -1;
+            break;
+        }
+    }
+    
+    return answer;
+}
