d1fb8a08a1a43a9ac1897d46110480cd56114c83
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-06 19:22:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int a = x;
+    int sum = 0;
+    
+    while (a > 0)
+    {
+        sum += a%10;
+        a/=10;
+    }
+    
+    answer = (x % sum == 0);
+    
+    return answer;
+}
