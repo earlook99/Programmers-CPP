461c551a58c558fdeb6f79061c59207c80dca9e1
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-11 19:31:44 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for (int a : d)
+    {
+        if (budget < a) break;
+        budget -= a;
+        answer++;
+    }
+    
+    return answer;
+}
