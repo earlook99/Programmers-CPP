27898d4f458bd348ba5d8d7cb6c28be0b0a77423
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-19 14:48:15 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 123456789;
+    answer = 0;
+    
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        answer +=  signs[i] ? absolutes[i] : -absolutes[i];   
+    }
+    
+    return answer;
+}
