f6b414835a77ac0f74896a9be8888214d4fdf51c
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-05-29 11:19:50 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> wallet, vector<int> bill) {
+    int answer = 0;
+    
+    if (wallet[0] < wallet[1]) swap(wallet[0], wallet[1]);
+    if (bill[0] < bill[1]) swap(bill[0], bill[1]);
+    
+    while (bill[1] > wallet[1] || bill[0] > wallet[0])
+    {
+        bill[0] /= 2;
+        answer++;
+
+        if (bill[0] < bill[1]) swap(bill[0], bill[1]);
+    }
+    
+    return answer;
+}
