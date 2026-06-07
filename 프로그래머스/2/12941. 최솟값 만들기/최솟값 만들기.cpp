ec65a8471c6ed0e6b5bfbb03b36f9247f1b68e51
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-06-07 12:04:59 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.09 ms, Memory: 3.78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<>());
+    answer = inner_product(A.begin(), A.end(), B.begin(), 0);
+    
+    return answer;
+}
