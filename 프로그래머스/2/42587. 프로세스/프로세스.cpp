d71a5bfbb83521c90f4b2219a698d7343ab80fd3
earlook99 <162270402+earlook99@users.noreply.github.com>
2026-06-22 09:27:59 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
+    
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        q.push({priorities[i], i});
+        pq.push(priorities[i]);
+    }
+    
+    while (!q.empty())
+    {
+        if (q.front().first < pq.top())
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        
+        else if (q.front().first == pq.top())
+        {
+            answer++;
+            if (q.front().second == location) break;
+            q.pop();
+            pq.pop();
+        }
+    }
+    
+    return answer;
+}
